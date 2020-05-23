336e3e6d9bd85b5ceaf0e5809135a09658364741
Kris Kersey <kkersey@dispersivegroup.com>
2020-05-23 16:26:35 -0400

    BINARY_FILE_TRANSFER breaks build. Won't boot.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -418,7 +418,7 @@
 #define TEMP_SENSOR_7 0
 #define TEMP_SENSOR_BED 1
 #define TEMP_SENSOR_PROBE 0
-//#define TEMP_SENSOR_CHAMBER 0
+#define TEMP_SENSOR_CHAMBER 0
 
 // Dummy thermistor constant temperature readings, for use with 998 and 999
 #define DUMMY_THERMISTOR_998_VALUE 25

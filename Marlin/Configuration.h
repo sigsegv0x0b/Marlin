c8e4e91cb5b4c5ff51cf426c06f5c52db7714356
Slava Polyakov <spolyakov@proofpoint.com>
2025-04-12 13:46:42 -0400

    fixing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1652,7 +1652,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 10, 10, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -45, -7, -3.1 }
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
